b35fa1112b34c83c43070e24d0d2c45e8e398571
thanhdat <thanhdat02112001pt@gmail.com>
2022-04-06 21:37:19 +0700

    init project

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Product\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use Illuminate\Support\Facades\Route;
+use Whoops\Run;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
